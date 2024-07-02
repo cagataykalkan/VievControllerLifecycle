e821054ee9a59ef572a1cfd85904bc5cc2fe11ce
cagataykalkan <c.kalkan203@gmail.com>
2024-07-02 17:59:22 +0300

    lifecycle methods - appDelegate - SceneDelegate methods added

---
+++ VievControllerLifecycle/ViewController2.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController2.swift
+//  VievControllerLifecycle
+//
+//  Created by Çağatay KALKAN on 2.07.2024.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
